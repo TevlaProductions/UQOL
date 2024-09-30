ae2a2cd1ea33d2a3d51e1129670c2d208655f658
tevla <ejghamilton@icloud.com>
2024-10-01 03:42:29 +1000

    Fixes Endcrystal false explosion

--- UQOL_Maven/src/main/java/qol/plugin/uQOL_Maven/Listeners/endcrystal.java
+++ UQOL_Maven/src/main/java/qol/plugin/uQOL_Maven/Listeners/endcrystal.java
@@ -4,20 +4,36 @@ import org.bukkit.World;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 
 public class endcrystal implements Listener {
     @EventHandler
+    public void onExplosion(EntityDamageEvent event) {
+        if (event.getEntity().getName().equals("Souliboi7")) {
+            if (event.getCause() == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) {
+                event.setCancelled(true);
+            }
+        }
+    }
+    @EventHandler
     public void onExplosion(EntityExplodeEvent event){
         Location location = event.getLocation();
         //
-        if (event.getEntity().equals(EntityType.END_CRYSTAL)) {
+        if (event.getEntityType().equals(EntityType.END_CRYSTAL)) {
             //Cancel the end crystal explosion
             event.setCancelled(true);
             //Makes a new explosion in the world position (explosion normally 6, now 4.8)
             World world = event.getEntity().getWorld();
             world.createExplosion(location, 4.8f, true, true);
+        }
+        if (event.getEntityType().equals(EntityType.TNT_MINECART)) {
+            //Cancel the end crystal explosion
+            event.setCancelled(true);
+            //Makes a new explosion in the world position (explosion normally 6, now 4.8)
+            World world = event.getEntity().getWorld();
+            world.createExplosion(location, 137f, true, true);
         }
     }
 }
