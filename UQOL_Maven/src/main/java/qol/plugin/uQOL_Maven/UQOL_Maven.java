dff396b3762960713467e07c7bb5415ce3d095fa
tevla <ejghamilton@icloud.com>
2024-10-02 03:14:13 +1000

    removes arki bullying, fixes item stack limiter

--- UQOL_Maven/src/main/java/qol/plugin/uQOL_Maven/UQOL_Maven.java
+++ UQOL_Maven/src/main/java/qol/plugin/uQOL_Maven/UQOL_Maven.java
@@ -14,8 +14,6 @@ public final class UQOL_Maven extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new Strength(), this);
         getServer().getPluginManager().registerEvents(new Gapples(), this);
         getServer().getPluginManager().registerEvents(new ItemStackLimiter(), this);
-        getServer().getPluginManager().registerEvents(new PlayerJoinEvent(),this);
-        getServer().getPluginManager().registerEvents(new PlayerMessage(), this);
     }
 
     @Override
